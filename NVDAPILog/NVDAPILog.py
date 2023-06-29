315c41d35e9d17fe6ddc100ea171476abca8ced4
Thomas Dressler <dressler.tom@gmail.com>
2023-06-29 15:39:49 -0400

    NVDAPILog.py changes:
    - Added variables to condense the code used to parse JSON CVE records.
    - Expanded CWE parsing to address additional non-standard record names.
    - Corrected the "config.yml" file path.
    - Removed extraneous comments & commented-out code.
    - Added smart comments to functions.

--- NVDAPILog/NVDAPILog.py
+++ NVDAPILog/NVDAPILog.py
@@ -1,17 +1,17 @@
-### This program uses the National Vulnerability Database's free API to 
-### retrieve Common Vulnerability & Exposure (CVE) records in JSON format.
-### Those records are then parsed & reformatted as lists, which are added as 
-### single entries to a SQLite database.  Each list contains the CVE ID, 
-### date published, date modified, vulnerability description, threat vector string, 
-### CVSS score, and CWE(s), if applicable.  Missing values are replaced with "NULL."
-### If a CVE record's CVSS score exceeds a certain value, a dated log file with 
-### record(s) is also generated.  The log files can be piped to an analytics platform.
+"""This program uses the National Vulnerability Database's free API to 
+retrieve Common Vulnerability & Exposure (CVE) records in JSON format.
+Those records are then parsed & reformatted as lists, which are added as 
+single entries to a SQLite database.  Each list contains the CVE ID, 
+date published, date modified, vulnerability description, threat vector string, 
+CVSS score, and CWE(s), if applicable.  Missing values are replaced with "NULL."
+If a CVE record's CVSS score exceeds a certain value, a dated log file with 
+record(s) is also generated.  The log files can be piped to an analytics platform."""
 
-### An accompanying "config.yaml" file may be used to set the
-### API's retrieval window & CVSS alert threshold.
-### Default values are 2 hours & CVSS 9.0, respectively.
+"""The SQLite database serves no purpose beyond a practical demonstration of coding ability."""
 
-### The SQLite database serves no purpose beyond a practical demonstration of coding ability.
+"""An accompanying "config.yaml" file may be used to set the
+API's retrieval window & CVSS alert threshold.
+Default values are 2 hours & CVSS 9.0, respectively."""
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ### This product uses the NVD API but is not endorsed or certified by the NVD. ###
@@ -26,16 +26,14 @@ import yaml # pip install pyyaml
 
 
 # Imports values from config file.
-with open("config.yml","r") as file:
+with open("Projects/NVDAPILog/config.yml","r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
     tDelta = config['timeDelta']
     aScore = config['alertScore']
-    # # Prints the current config file values.
-    # print(tDelta, aScore)
 
 
-# Creates a SQLite table for CVE records if none exists.
 def makeCVETable():
+    """Creates a SQLite table for CVE records if none exists."""
     connection = sqlite3.connect('CVEdatabase.db')
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS NVD_CVEs (CVE_ID STRING PRIMARY KEY NOT NULL,
@@ -49,8 +47,8 @@ def makeCVETable():
     connection.close()
 
 
-# API request for any CVE updates within the timedelta value.
 def NVDAPI():
+    """Submits API request for any CVE updates within the timedelta value."""
     url = 'https://services.nvd.nist.gov/rest/json/cves/2.0/?lastModStartDate='
     pasttime = datetime.datetime.now() - datetime.timedelta(hours=tDelta)
     nowtime = datetime.datetime.now()
@@ -85,66 +83,71 @@ def NVDAPI():
             print()
 
 
-# Parses JSON data into table entries.
 def parseJSON(element):
+    """Parses JSON data into a list of values."""
     CVEdbEntry = []
-    # Check is there's a CVE entry in the JSON return.
+    # Checks if there's a CVE entry in the JSON return.
     if 'cve' not in element:
         pass
-    # Add the CVE name, published date, and lastModified date to CVEdbEntry.
+    # Adds the CVE name, published date, and lastModified date to CVEdbEntry.
     else:
         CVEdbEntry.append(element['cve']['id'])
         CVEdbEntry.append(element['cve']['published'])
         CVEdbEntry.append(element['cve']['lastModified'])
-        # Add the CVE text description to CVEdbEntry.
+        # Adds the CVE text description to CVEdbEntry.
         if 'descriptions' not in element['cve']:
             CVEdbEntry.append('NULL')
         elif 'value' not in element['cve']['descriptions'][0]:
             CVEdbEntry.append('NULL')
         else:
-            CVEdbEntry.append(element['cve']['descriptions'][0]['value'])
-        # Add the CVSS vectorString & baseScore to CVEdbEntry.
+            CVEdbEntry.append(element['cve']['descriptions'][0]['value'])       
+        # Sets variables to reduce program size.
+        isV31 = 'cvssMetricV31' in element['cve']['metrics'] ### Might be redundant?  Only used on line 142.
+        isntV31 = 'cvssMetricV31' not in element['cve']['metrics']
+        isV30 = 'cvssMetricV30' in element['cve']['metrics']
+        isntV30 = 'cvssMetricV30' not in element['cve']['metrics']
+        isV2 = 'cvssMetricV2' in element['cve']['metrics']
+        isntV2 = 'cvssMetricV2' not in element['cve']['metrics']    
+        # Adds the CVSS vectorString & baseScore to CVEdbEntry.  
+        # If no CVSS metrics found.
         if 'metrics' not in element['cve']:
             CVEdbEntry.extend(['NULL', 'NULL'])
-        # If no CVSS metric found.
-        elif 'cvssMetricV31' not in element['cve']['metrics'] and 'cvssMetricV30' not in element['cve']['metrics']\
-            and 'cvssMetricV2' not in element['cve']['metrics']:
-            CVEdbEntry.extend(['NULL', 'NULL'])
-        # If only CVSSv2 metric found.
-        elif 'cvssMetricV31' not in element['cve']['metrics'] and 'cvssMetricV30' not in element['cve']['metrics']\
-            and 'cvssMetricV2' in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV2'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseScore']])
-        # If only CVSSv3.0 metric found.
-        elif 'cvssMetricV31' not in element['cve']['metrics'] and 'cvssMetricV30' in element['cve']['metrics']\
-            and 'cvssMetricV2' not in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']])
-        # If only CVSSv3.1 metric found.
-        elif 'cvssMetricV31' in element['cve']['metrics'] and 'cvssMetricV30' not in element['cve']['metrics']\
-            and 'cvssMetricV2' not in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
-        # If both CVSSv3.1 & CVSSv2 metrics found.
-        elif 'cvssMetricV31' in element['cve']['metrics'] and 'cvssMetricV30' not in element['cve']['metrics']\
-            and 'cvssMetricV2' in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
-        # If both CVSSv3.1 & CVSSv3.0 metrics found.
-        elif 'cvssMetricV31' in element['cve']['metrics'] and 'cvssMetricV30' in element['cve']['metrics']\
-            and 'cvssMetricV2' not in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
-        # If both CVSSv3.0 & CVSSv2 metrics found.
-        elif 'cvssMetricV31' not in element['cve']['metrics'] and 'cvssMetricV30' in element['cve']['metrics']\
-            and 'cvssMetricV2' in element['cve']['metrics']:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']])
-        # If all three CVSS metrics found.
-        else:
-            CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
-                               element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
-        # Add CWE name(s) to CVEdbEntry.
+        # If no CVSSv3.1 metric found:       
+        elif isntV31:
+            # If no other CVSS metrics found.
+            if isntV30 and isntV2:
+                CVEdbEntry.extend(['NULL', 'NULL'])
+            # If only CVSSv2 metric found.
+            elif isntV30 and isV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV2'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseScore']])
+            # If only CVSSv3.0 metric found.
+            elif isV30 and isntV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['vectorString'],\
+                                element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']])
+            # If both CVSSv3.0 & CVSSv2 metrics found.
+            elif isV30 and isV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']])
+        # If CVSSv3.1 metric is found.
+        elif isV31:  ### Switch to "else:"?
+            # If only CVSSv3.1 metric is found.
+            if isntV30 and isntV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
+            # If both CVSSv3.1 & CVSSv2 metrics found.
+            elif isntV30 and isV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
+            # If both CVSSv3.1 & CVSSv3.0 metrics found.
+            elif isV30 and isntV2:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
+            # If all three CVSS metrics found.     
+            else:
+                CVEdbEntry.extend([element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],\
+                                   element['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']])
+        # Adds CWE name(s) to CVEdbEntry.
         if 'weaknesses' not in element['cve']:
             CVEdbEntry.append('NULL')
         elif len(element['cve']['weaknesses']) == 0:
@@ -162,17 +165,19 @@ def parseJSON(element):
         else:
             CWEValues = []
             for i in range(len(element['cve']['weaknesses'])):
-                CWEValues.append(element['cve']['weaknesses'][i]['description'][0]['value'])
+                if element['cve']['weaknesses'][i]['description'][0]['value'] == 'NVD-CWE-noinfo':
+                    CVEdbEntry.append('NULL')
+                elif element['cve']['weaknesses'][i]['description'][0]['value'] == 'NVD-CWE-Other':
+                    CVEdbEntry.append('NULL')
+                else:
+                    CWEValues.append(element['cve']['weaknesses'][i]['description'][0]['value'])
             CWEValues = '; '.join(CWEValues)
             CVEdbEntry.append(CWEValues)
-    # # Prints each record after it's reformatted.
-    # print(CVEdbEntry)
-    # print()
     return CVEdbEntry
 
 
-# Adds a CVE entry to the SQLite table.
 def addCVE(CVEdbEntry):
+    """Adds a CVE entry to the SQLite table."""
     connection = sqlite3.connect('CVEdatabase.db')
     cursor = connection.cursor()
     cursor.executemany('''INSERT OR REPLACE INTO NVD_CVEs (CVE_ID,
@@ -186,20 +191,20 @@ def addCVE(CVEdbEntry):
     connection.close()
 
 
-# Generates a log file of selected CVEs.
 def createLog(CVEdbEntry):
+    """Generates a log file of selected CVEs."""
     logging.basicConfig(filename=datetime.datetime.now().strftime('CVEAlerts_%Y%m%d_%H%M%S.log'), encoding='utf-8', level=logging.INFO)
     logging.info(CVEdbEntry)
 
 
-# The main function.
 def main():
+    """The main function."""
     makeCVETable()
     NVDAPI()
     print('Check complete -', datetime.datetime.now())
     print()
 
 
-# Calls the main function.
 if __name__ == '__main__':
+    """Calls the main function."""
     main()
